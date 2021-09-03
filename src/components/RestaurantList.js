54a10f501395ffc829faf151ef7f19441be43b13
Lorian Atashi <lorian.atashi1903@gmail.com>
2021-09-03 21:31:14 +0200

    added restaurantlist render and display test

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,5 +1,18 @@
-const RestaurantList = () => {
-  return <div>RestaurantList</div>;
+import {useEffect} from 'react';
+
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+  return (
+    <div>
+      <ul>
+        {restaurants.map(restaurant => (
+          <li key={restaurant.id}>{restaurant.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RestaurantList;
