41f0ba37aff8b93905dd26b04340764999d8e0f9
Lorian Atashi <lorian.atashi1903@gmail.com>
2021-09-03 17:04:56 +0200

    Added config for API

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const API_URL = 'https://outside-in-dev-api.herokuapp.com/YOUR-API-KEY/restaurants';
+const API_KEY = 'US5mln1StgswcASthwbGAoFO1jP6B2et';
+
+export default {
+  API_URL,
+};
---
+++ cypress/integration/listing-restaurants.spec.js
@@ -0,0 +1,24 @@
+// Config import
+import * as settings from '../../config/config';
+
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url: `${settings.API_URL}${settings.API_KEY}`,
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
